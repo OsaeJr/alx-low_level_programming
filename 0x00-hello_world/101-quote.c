cf4a1ed7a2e4b1a6765fe4c219b911f240ea3007
OsaeJr <samueladjar11@gmail.com>
2023-06-15 18:35:49 -0700

    Edit task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry Point
  * Return: 1 
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
