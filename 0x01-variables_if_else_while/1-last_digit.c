a1b03e6daba714fba9b332c5de8e7d4b42af6827
OsaeJr <samueladjar11@gmail.com>
2023-06-16 15:08:23 -0700

    Complete the source code in order to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,8 @@
 /**
  * main - Entry point
  * Return: Always 0
- * /
-/ * betty style doc for function main goes there */
+ */
+/* betty style doc for function main goes there */
 
 int main(void)
 {
