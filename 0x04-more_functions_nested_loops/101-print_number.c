f99130b5726d7e36b8a2f4c0d2754115c8453dc5
OsaeJr <samueladjar11@gmail.com>
2023-06-24 06:16:26 -0700

    Edit task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,39 +6,14 @@
  */
 void print_number(int n)
 {
-	if (n >= 0 && n <= 9)
-	{
-		_putchar(n + '0');
-	}
-	if (n > 9 && n < 100)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n > 99 && n < 1000)
-	{
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n > 999 && n < 10000)
-	{
-		_putchar(((n / 1000) % 10) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	if (n < 0 && n >= -9)
+	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-		_putchar(n + '0');
 	}
-	if (n < -9 && n > -100)
+	if (n > 9)
 	{
-		_putchar('-');
-		n = -n;
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		print_number(n / 10);
 	}
+	_putchar((n % 10) + '0');
 }
