8ffaa9b4c7ea158ac94722fe1e40f772264fa889
OsaeJr <samueladjar11@gmail.com>
2023-07-20 16:53:49 -0700

    Edit file

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -19,21 +19,16 @@ void print_all(const char * const format, ...)
 		case 'c':
 			printf("%c", va_arg(args, int));
 			break;
-
 		case 'i':
 			printf("%d", va_arg(args, int));
 			break;
-
 		case 'f':
 			printf("%f", va_arg(args, double));
 			break;
-
 		case 's':
 			str = va_arg(args, char *);
 			if (str == NULL)
 				printf("(nil)");
-			else
-				printf("%s", str);
 			break;
 	}
 	ptr++;
