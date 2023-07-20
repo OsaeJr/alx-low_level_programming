30f6111b58960baa1651da4237f7a30e99422cf3
OsaeJr <samueladjar11@gmail.com>
2023-07-19 18:27:45 -0700

    Add guards to header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,8 @@
+#ifndef HEADER_
+#define HEADER_
+#include <stddef.h>
 int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
+#endif
