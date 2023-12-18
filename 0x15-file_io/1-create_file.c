0d2a598a1cb2dc3c7e345b77a4201a6973015d4e
OsaeJr <samueladjar11@gmail.com>
2023-12-18 04:26:26 -0800

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,37 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * create_file - Creates a file with specified perm and writes content to it.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
+ * create_file - Creates a file with specified pmissn and writes content to it.
+ * @file_name: The name of the file to create.
+ * @content: A NULL-terminated string to write to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *file_name, char *content)
 {
-	size_t content_length = 0;
-	int file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	ssize_t bytes_written = write(file_descriptor, text_content, content_length);
+	int file_descriptor;
+	int num_letters;
+	int result;
 
-	if (filename == NULL)
+	if (!file_name)
 		return (-1);
+
+	file_descriptor = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_descriptor == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		while (text_content[content_length] != '\0')
-			content_length++;
+	if (!content)
+		content = "";
 
-		if (bytes_written == -1)
-		{
-			close(file_descriptor);
-			return (-1);
-		}
-	}
+	for (num_letters = 0; content[num_letters]; num_letters++)
+		result = write(file_descriptor, content, num_letters);
+
+	if (result == -1)
+		return (-1);
 
 	close(file_descriptor);
+
 	return (1);
 }
 
