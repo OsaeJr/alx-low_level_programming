f0fd694ed0d467a2ae91c7b8c0b2b035a045b17b
OsaeJr <samueladjar11@gmail.com>
2023-12-18 04:18:56 -0800

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 /**
- * create_file - Creates a file with specified permissions and writes content to it.
+ * create_file - Creates a file with specified perm and writes content to it.
  * @filename: The name of the file to create.
- * @text_content: A NULL terminated string to write to the file.
+ * @text_content: A NULL-terminated string to write to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
@@ -20,22 +19,21 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (file_descriptor == -1)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
-		size_t content_length = 0;
 		while (text_content[content_length] != '\0')
-	{
-		content_length++;
+			content_length++;
+
+		if (bytes_written == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
 	}
 
-        if (bytes_written == -1)
-	{
-		close(file_descriptor);
-		return (-1);
-	}
-	}
+	close(file_descriptor);
+	return (1);
+}
 
-		close(file_descriptor);
-		return (1);}
