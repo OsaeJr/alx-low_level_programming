427ed490ccee7e9f9bad847d169d980ab2fb2054
OsaeJr <samueladjar11@gmail.com>
2023-12-18 04:30:58 -0800

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,30 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file with specified pmissn and writes content to it.
- * @file_name: The name of the file to create.
- * @content: A NULL-terminated string to write to the file.
+ * create_file - Creates a file.
+ * @file_name: A pointer to the name of the file to create.
+ * @content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *file_name, char *content)
 {
-	int file_descriptor;
-	int num_letters;
-	int result;
+	int file_descriptor, bytes_written, length = 0;
 
-	if (!file_name)
+	if (file_name == NULL)
 		return (-1);
 
-	file_descriptor = open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (content != NULL)
+	{
+		for (length = 0; content[length];)
+			length++;
+	}
 
-	if (file_descriptor == -1)
-		return (-1);
+	file_descriptor = open(file_name, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_written = write(file_descriptor, content, length);
 
-	if (!content)
-		content = "";
-
-	for (num_letters = 0; content[num_letters]; num_letters++)
-		result = write(file_descriptor, content, num_letters);
-
-	if (result == -1)
+	if (file_descriptor == -1 || bytes_written == -1)
 		return (-1);
 
 	close(file_descriptor);
